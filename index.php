633a9ed2d4e8b249a7d29b6069295def94345f0e
Olu <theoafactor@gmail.com>
2023-03-29 15:47:01 +0100

    tHIS IS HELLO

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
 echo "This is working";
+
+echo "<div>Hello</div>";
+echo "<div>Hello</div>";
+echo "<div>Hello</div>";
+echo "<div>Hello</div>";
+echo "<div>Hello
+
+
+<script>
+    alert('working')
+</script>
+</div>";
